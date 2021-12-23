4363736990f08db678eb57319c7cdfd9aaa7d349
drapnet <59938697+drapnet@users.noreply.github.com>
2021-12-23 19:47:25 +0100

    Add files via upload

---
+++ sourceFrtn./variablesbaro.f90
@@ -0,0 +1,13 @@
+MODULE variablesbaro
+USE mod_kinds
+implicit none
+SAVE
+integer :: tim, L, M, N
+
+real(r8),DIMENSION(:,:,:,:),ALLOCATABLE :: u, v
+real(r8),DIMENSION(:,:,:),ALLOCATABLE ::h_5,ubar,vbar
+
+
+
+
+END MODULE variablesbaro
